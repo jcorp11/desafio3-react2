c024b0e9279e1f17930889ad2f28e611a3de1a03
jcorp11 <joaco.011@gmail.com>
2024-01-05 20:59:10 -0300

    navbar y home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+
+import Home from "./views/Home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/favoritos" element={<Favorites />} /> */}
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const setActiveClass = ({ isActive }) => (isActive ? "active" : null);
+
+  return (
+    <nav className="navbar">
+      <section className="icon">icon</section>
+      <section className="links">
+        <NavLink className={setActiveClass} to="/">
+          Home
+        </NavLink>
+        <NavLink className={setActiveClass} to="/pokemon">
+          Pokemones
+        </NavLink>
+      </section>
+    </nav>
+  );
+};
+export default Navbar;
