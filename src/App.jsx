d1008cb26b65af397229b805f82ed3b8320f76cb
jcorp11 <joaco.011@gmail.com>
2024-01-17 00:28:49 -0300

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Router from "./routes/Router";
-
-import Home from "./views/Home";
-import Choose from "./views/Choose";
-import Poke from "./views/Poke";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/choosePokemon" element={<Choose />} />
-        <Route path="/pokemon/:id" element={<Poke />} />
-      </Routes> */}
       <Router />
     </>
   );
